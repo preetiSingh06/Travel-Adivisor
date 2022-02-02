bc927a31d4fbf503e9ace3859281455351b5ea80
Preeti Singh <preetisinghkvs3@gmail.com>
2022-02-03 01:36:46 +0530

    Final commit

---
+++ src/api/index.js
@@ -0,0 +1,50 @@
+
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          'x-rapidapi-key': '84c8c82c6dmsh1540e1ed838a3dep1ace7ejsn8e68c576f94c'
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get(
+      "https://community-open-weather-map.p.rapidapi.com/weather",
+      {
+        params: {
+          lat: lat,
+          lon: lng,
+        },
+        headers: {
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          'x-rapidapi-key': '84c8c82c6dmsh1540e1ed838a3dep1ace7ejsn8e68c576f94c'
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
